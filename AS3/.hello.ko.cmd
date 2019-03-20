5d49a267d3cb12eab3cc102cabf6f375b1d13c7b
Yu Peng <tanyupeng1994@gmail.com>
2019-03-20 14:39:48 +0800

    add files built from hello.c

---
+++ AS3/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/yupengtan/Desktop/CS5250/AS3/hello.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/yupengtan/Desktop/CS5250/AS3/hello.ko /home/yupengtan/Desktop/CS5250/AS3/hello.o /home/yupengtan/Desktop/CS5250/AS3/hello.mod.o ;  true
---
+++ AS3/.tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/yupengtan/Desktop/CS5250/AS3/hello.ko
+/home/yupengtan/Desktop/CS5250/AS3/hello.o
+
